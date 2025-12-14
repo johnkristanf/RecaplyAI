107511c04908eec827c101546fb08312ecfb1b97
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-12-14 10:30:24 +0800

    project initialized

---
+++ client/app/pages/recordings.tsx
@@ -0,0 +1,15 @@
+export function meta() {
+  return [
+    { title: "Recordings" },
+    { name: "description", content: "Browse all your recordings." },
+  ];
+}
+
+export default function RecordingsPage() {
+  return (
+    <main>
+      <h1>Recordings</h1>
+      {/* Add your recordings list or components here */}
+    </main>
+  );
+}
